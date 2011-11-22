224175324a533846e6fc53f9cc4a14567b7fefc2
Marcus Wallstersson <wallstersson@hotmail.com>
2011-11-22 17:56:54 +0100

    ClientPackager

---
+++ ClientBytePacker.java
@@ -0,0 +1,69 @@
+
+public class ClientBytePacker {
+	
+	byte[] bytePackage = new byte[10];
+	
+	final byte LOGIN = 0x00;
+	final byte BALANCE = 0x01;
+	final byte WITHDRAWAL = 0x02;
+	final byte DEPOSIT = 0x03;
+	
+	public void ClientBytePacker() {
+	}
+
+	public byte[] login(int cardnumber, int pin){
+		bytePackage[0] = LOGIN;
+		bytePackage[1] = (byte)(pin >> 8);
+		bytePackage[2] = (byte)(pin >> 0);
+		bytePackage[3] = (byte)(cardnumber >> 48);
+		bytePackage[4] = (byte)(cardnumber >> 40);
+		bytePackage[5] = (byte)(cardnumber >> 32);
+		bytePackage[6] = (byte)(cardnumber >> 24);
+		bytePackage[7] = (byte)(cardnumber >> 16);
+		bytePackage[8] = (byte)(cardnumber >> 8);
+		bytePackage[9] = (byte)(cardnumber >> 0);
+		return bytePackage;	
+	}
+	
+	public byte[] balance(){
+		bytePackage[0] = BALANCE;
+		bytePackage[1] = (byte)(0);
+		bytePackage[2] = (byte)(0);
+		bytePackage[3] = (byte)(0);
+		bytePackage[4] = (byte)(0);
+		bytePackage[5] = (byte)(0);
+		bytePackage[6] = (byte)(0);
+		bytePackage[7] = (byte)(0);
+		bytePackage[8] = (byte)(0);
+		bytePackage[9] = (byte)(0);
+		return bytePackage;	
+	}
+	
+	public byte[] withdrawal(int securityCode, long amount){
+		bytePackage[0] = WITHDRAWAL;
+		bytePackage[1] = (byte)(securityCode);
+		bytePackage[2] = (byte)(0);
+		bytePackage[3] = (byte)(0);
+		bytePackage[4] = (byte)(0);
+		bytePackage[5] = (byte)(0);
+		bytePackage[6] = (byte)(0);
+		bytePackage[7] = (byte)(0);
+		bytePackage[8] = (byte)(0);
+		bytePackage[9] = (byte)(0);
+		return bytePackage;	
+	}
+	
+	public byte[] deposit(int securityCode, long amount){
+		bytePackage[0] = WITHDRAWAL;
+		bytePackage[1] = (byte)(securityCode);
+		bytePackage[2] = (byte)(amount >> 56);
+		bytePackage[3] = (byte)(amount >> 48);
+		bytePackage[4] = (byte)(amount >> 40);
+		bytePackage[5] = (byte)(amount >> 32);
+		bytePackage[6] = (byte)(amount >> 24);
+		bytePackage[7] = (byte)(amount >> 16);
+		bytePackage[8] = (byte)(amount >> 8);
+		bytePackage[9] = (byte)(amount >> 0);
+		return bytePackage;	
+	}
+}
