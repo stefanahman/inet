c88bb7bc8ed1c25c3f3069fe0f8fee236f76bd9f
Stefan Åhman <stefan.ahman@gmail.com>
2011-11-30 20:30:54 +0100

    sec codes fix

--- AccountDatabase.java
+++ AccountDatabase.java
@@ -4,11 +4,11 @@ import java.util.*;
 public class AccountDatabase {
 	
 	static LinkedList<BankAccount> list = new LinkedList<BankAccount>();
-
+	
 	public static void readUsers(){
-		list.add(new BankAccount(1234123412341234L, 1000, 1234));
-		list.add(new BankAccount(1111111111111111L, 2000, 1111));
-		list.add(new BankAccount(2222222222222222L, 3000, 2222));
+		list.add(new BankAccount(1234123412341234L, 1000, 1234, null));
+		list.add(new BankAccount(1111111111111111L, 2000, 1111, null));
+		list.add(new BankAccount(2222222222222222L, 3000, 2222, null));
 	}
 
 	public static BankAccount login(long cardnumber, int pin){
--- BankAccount.java
+++ BankAccount.java
@@ -1,15 +1,38 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class BankAccount {
 
 	long cardnumber, balance;
 	int pin;
+	List<Integer> seccodes = new ArrayList<Integer>();
 
-	public BankAccount(long cardnumber, long balance, int pin) {
+	public BankAccount(long cardnumber, long balance, int pin, List<Integer> seccodes) {
 		this.cardnumber = cardnumber;
 		this.balance = balance;
 		this.pin = pin;
+		this.seccodes = generateSecurityCodes();
 	}
 
+	public List<Integer> generateSecurityCodes(){
+		for(int i = 0,j = 1; j < 100;i++,j+=2)
+		{
+			seccodes.add(i, j);
+		}
+		return seccodes;
+	}
+	
+	public void removeSecurityCode(int seccode){
+		Iterator<Integer> iterator = seccodes.iterator();
+		while (iterator.hasNext()) {
+			int current = iterator.next();
+			if (current == seccode){
+				iterator.remove();
+				break;
+			}
+		}
+	}
 
 	public long getBalance() {
 		return balance;
@@ -18,6 +41,7 @@ public class BankAccount {
 	public boolean withdrawal(long amount, int securityCode) {
 		if(amount <= this.balance && validateCode(securityCode)) {
 			this.balance = this.balance - amount;
+			removeSecurityCode(securityCode);
 			return true;
 		}
 		return false;
@@ -26,12 +50,14 @@ public class BankAccount {
 	public boolean deposit(long amount, int securityCode) {
 		if(validateCode(securityCode)) {
 			this.balance = this.balance + amount;
+			removeSecurityCode(securityCode);
 			return true;
 		}
 		return false;
 	}
 
 	public boolean validateCode(int securityCode) {
-		return ((securityCode < 100) && (securityCode > 0) && (securityCode % 2 != 0));
+		return seccodes.contains(securityCode);
+		//return ((securityCode < 100) && (securityCode > 0) && (securityCode % 2 != 0));
 	}
 }
